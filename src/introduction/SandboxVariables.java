02c8c58f974804436307ecaacedb095be9869c9c
Herbert Quijano <herbert.quijano@gmail.com>
2024-02-28 17:28:17 -0600

    first commit

---
+++ src/introduction/SandboxVariables.java
@@ -0,0 +1,14 @@
+package introduction;
+
+
+public class SandboxVariables {
+    static int num1;
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println("Number 1: " + num1);
+
+
+    }
+}
